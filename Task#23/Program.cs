debbb357a72bb76d8af6c2cce9197a7cfb37475c
AnnaGoko <anna.gordienko@myrambler.ru>
2022-12-27 00:12:33 +0300

    Finished Task#3

---
+++ Task#23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Задание 23: Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int length = num;
+for ( num = 1; num < length; num++)
+{
+    Console.WriteLine($"{Math.Pow(num,3)}");
+}
+Console.WriteLine($"{Math.Pow(num,3)}");

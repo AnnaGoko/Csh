105b2dae11f381fd827c3da8d738be062303ec64
AnnaGoko <anna.gordienko@myrambler.ru>
2022-12-30 14:48:11 +0300

    finished task#4

---
+++ Task#29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+Console.WriteLine("Задание 29: Задайте длину массива: ");
+int length = int.Parse(Console.ReadLine());
+int [] NewArray = new int [length];
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("Введите {0} элемент массива: ", i);
+    NewArray[i] = int.Parse(Console.ReadLine());          
+}
+
+Console.WriteLine ("NewArray = { ");
+
+for (int a = 0; a < length; a++)
+{
+    Console.WriteLine (NewArray [a]);
+}
+Console.WriteLine ("}");

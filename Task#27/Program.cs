4194081215c1217758ee1a84f3dc61786f2ecb31
AnnaGoko <anna.gordienko@myrambler.ru>
2022-12-30 14:48:02 +0300

    finished task#4

---
+++ Task#27/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Задание 27: Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int answer = 0;
+while (num > 0) {
+    answer = answer + num % 10;
+    num /= 10;
+}
+Console.WriteLine($"{answer}");
